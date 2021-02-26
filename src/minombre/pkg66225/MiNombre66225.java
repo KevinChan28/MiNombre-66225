b54718544ec3392bc382858d371602f372fbfecf
USER <USER@192.168.1.117>
2021-02-25 20:10:16 -0600

    Primer commit

---
+++ src/minombre/pkg66225/MiNombre66225.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minombre.pkg66225;
+
+/**
+ *
+ * @author USER
+ */
+public class MiNombre66225 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre="KEVIN ADRIAN CHAN GUZMAN";
+        System.out.println("Mi nombre es: "+nombre);
+                
+        // TODO code application logic here
+    }
+    
+}
